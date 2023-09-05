912dfa68eb8da432da9e6fba70cecf01e6cd5d80
Mariahseggebruch <mariahseggebruch@gmail.com>
2023-09-05 17:27:44 +0000

    task 1

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,12 @@
+export default function taskBlock(trueOrFalse) {
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const newTask = true;
+    const newTask2 = false;
+    return [newTask2, newTask];
+    }
+
+  return [task, task2];
+}
